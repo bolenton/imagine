34b95d7ed1e5e3b821d5f6a3fef194df67275b32
Taurus Omejia <bolenton@gmail.com>
2016-02-10 16:57:16 -0600

    fixed loading button, refresh issue

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
 
 $('.search').click(function () {
-    var search = $('#userInput').val();
-   
+    $(".main-body").empty();
     $('.search').addClass('is-loading');
+    var search = $('#userInput').val();
 
     $.ajax({
         type: "GET",
         url: "http://www.reddit.com/r/aww/search.json?q=" + search + "&limit=100&restrict_sr=true",
         success: function (response) {
+
             $('.text').html('');
             for (var i = 0; i < response.data.children.length; i++) {
-               response.data.children.thumbnail
-                
-                var img = "<img  class='majicImg' src='" + 
-                response.data.children[i].data.thumbnail + "'/>";
-                $(".main-body").append(img);                
+                response.data.children.thumbnail
+
+                var img = "<img  class='majicImg' src='" +
+                    response.data.children[i].data.thumbnail + "'/>";
+                $(".main-body").append(img);
             }
+            $('.search').removeClass('is-loading');
         }
     });
 });
+
