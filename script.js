7fac35ba472cbafedc10ae945f7c04cb36ea1f6c
Taurus Omejia <bolenton@gmail.com>
2016-02-10 17:31:31 -0600

    more image options

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ $('.search').click(function () {
 
     $.ajax({
         type: "GET",
-        url: "http://www.reddit.com/r/aww/search.json?q=" + search + "&limit=100&restrict_sr=true",
+        url: "http://www.reddit.com/r/pics/search.json?q=" + search + "&limit=100&restrict_sr=true",
         success: function (response) {
 
             $('.text').html('');
             for (var i = 0; i < response.data.children.length; i++) {
                 response.data.children.thumbnail
 
-                var img = "<img  class='majicImg' src='" +
-                    response.data.children[i].data.thumbnail + "'/>";
+                var img = response.data.children[i].data.thumbnail;
+                var defaultImg = "default.jpg"
+                if (img.length < 10)
+                {
+                     img = "<img  class='majicImg' src='" + defaultImg + "'/>";
+                }
+                else
+                {
+                    img = "<img  class='majicImg' src='" + response.data.children[i].data.thumbnail + "'/>";
+                }
                 $(".main-body").append(img);
+                
             }
             $('.search').removeClass('is-loading');
         }
