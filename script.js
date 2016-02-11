2a98f888140cc2c7550ea5a708ed304406da3584
Taurus Omejia <bolenton@gmail.com>
2016-02-11 11:33:31 -0600

    added listener for enter key press

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+//Listen for enter button press
+$("#userInput").on("keyup", function (e) {
+    if (e.keyCode == '13') {
+        e.preventDefault();
+        $(this).next("a.search").trigger("click");
+    }
+});
+
+//listens for search button
 $('.search').click(function () {
     $(".main-body").empty();
     $('.search').addClass('is-loading');
     var search = $('#userInput').val();
-
+    
+    //Gets results fromn reddit and displays it.
     $.ajax({
         type: "GET",
         url: "http://www.reddit.com/r/pics/search.json?q=" + search + "&limit=100&restrict_sr=true",
         success: function (response) {
-
+            
             $('.text').html('');
+            
             for (var i = 0; i < response.data.children.length; i++) {
-                response.data.children.thumbnail
-
                 var img = response.data.children[i].data.thumbnail;
                 var defaultImg = "default.jpg"
+                
                 if (img.length < 10) {
                     img = "<img  class='majicImg' src='" + defaultImg + "'/>";
                 }
@@ -23,8 +33,7 @@ $('.search').click(function () {
                 
                 $(".main-body").append(img);
             }
-            
-            $('.search').removeClass('is-loading');        
+            $('.search').removeClass('is-loading');
         }
     });
 });
